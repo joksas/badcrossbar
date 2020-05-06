9c773db4a622014e1a6c1c01ad2d8e0a112eda0a
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 18:05:16 +0100

    create fill_r

--- crossbar.py
+++ crossbar.py
@@ -1,3 +1,5 @@
+from scipy.sparse import lil_matrix, csr_matrix
+
 def currents(voltages, resistances, r_i=0):
     (num_inputs, num_outputs) = resistances.shape
     num_examples = voltages.shape[1]
@@ -7,3 +9,13 @@ def currents(voltages, resistances, r_i=0):
     currents = extract_currents(i)
 
     return currents
+
+
+def fill_r(resistances, r_i):
+    num_resistances = resistances.size
+    r_shape = tuple(3*num_resistances for _ in range(2))
+    r = lil_matrix(r_shape)
+    r = kvl(r, resistances, r_i)
+    r = kcl(r)
+    r = same_branches(r)
+    return r
