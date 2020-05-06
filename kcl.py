aa32186bbb01f496871d6d4f31550f12e57a3e1f
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 20:48:46 +0100

    create fill_right

--- kcl.py
+++ kcl.py
@@ -25,3 +25,23 @@ def fill_left(r, resistances):
     devices[row, column] = 1
 
     return r
+
+
+def fill_right(r, resistances):
+    (num_rows, num_columns) = resistances.shape
+    devices = r[2*resistances.size:3*resistances.size, :resistances.size]
+    vertical = r[2*resistances.size:3*resistances.size, 2*resistances.size:3*resistances.size]
+
+    row = np.repeat(np.arange(1, num_rows), num_columns)
+    column = np.tile(np.arange(num_columns), num_rows-1)
+    devices[row*num_columns+column, row*num_columns+column] = -1
+    vertical[row*num_columns+column, (row-1)*num_columns+column] = -1
+    vertical[row*num_columns+column, row*num_columns+column] = 1
+
+    # same branches
+    row = np.arange(num_columns)
+    column = np.arange(num_columns)
+    devices[row, column] = -1
+    vertical[row, column] = 1
+
+    return r
