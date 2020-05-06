ec153c28c7451da8c240959ecf4b68334bb19d57
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 19:39:06 +0100

    create fill_vertical

--- kvl.py
+++ kvl.py
@@ -22,3 +22,14 @@ def fill_horizontal(r, r_i, resistances):
             horizontal[row*num_columns+column, row*num_columns:row*num_columns+column+1] = -r_i
 
     return r
+
+
+def fill_vertical(r, r_i, resistances):
+    vertical = r[:resistances.size, 2*resistances.size:3*resistances.size]
+    (num_rows, num_columns) = resistances.shape
+
+    for row in range(num_rows):
+        for column in range(num_columns):
+            vertical[row*num_columns+column, row*num_columns+column::num_columns] = -r_i
+
+    return r
