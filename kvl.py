d849c5601facc29eb0a07dc46f76db343211bfd6
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 19:02:12 +0100

    create fill_resistances

--- kvl.py
+++ kvl.py
@@ -1,5 +1,14 @@
+import numpy as np
+
+
 def apply(r, resistances, r_i):
     r = fill_resistances(r, resistances)
     r = fill_horizontal(r, r_i)
     r = fill_vertical(r, r_i)
+    return r
+
+
+def fill_resistances(r, resistances):
+    num_resistances = int(r.shape[0]/3)
+    np.fill_diagonal(r[:num_resistances, :num_resistances], resistances.flatten())
     return r
