abb79a8f34283aa159b211a6ba9d73ad729f050a
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 18:05:27 +0100

    rename fill_b to fill_v

--- crossbar.py
+++ crossbar.py
@@ -4,7 +4,7 @@ def currents(voltages, resistances, r_i=0):
     (num_inputs, num_outputs) = resistances.shape
     num_examples = voltages.shape[1]
     r = fill_r(resistances, r_i)
-    v = fill_b(voltages)
+    v = fill_v(voltages)
     i = solve(r, v)
     currents = extract_currents(i)
 
