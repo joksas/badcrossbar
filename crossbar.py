c5ced202d5b16427c355d8e9f52dd4ec25e40f87
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 19:02:25 +0100

    use kvl

--- crossbar.py
+++ crossbar.py
@@ -1,4 +1,5 @@
 from scipy.sparse import lil_matrix, csr_matrix
+import kvl
 
 def currents(voltages, resistances, r_i=0):
     (num_inputs, num_outputs) = resistances.shape
@@ -15,6 +16,6 @@ def fill_r(resistances, r_i):
     num_resistances = resistances.size
     r_shape = tuple(3*num_resistances for _ in range(2))
     r = lil_matrix(r_shape)
-    r = kvl(r, resistances, r_i)
+    r = kvl.apply(r, resistances, r_i)
     r = kcl(r)
     return r
