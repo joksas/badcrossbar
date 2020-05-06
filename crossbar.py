be2d87569ed48ae3b149b6f0770ecc5479695fdc
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-06 17:32:28 +0100

    make variables lowercase

--- crossbar.py
+++ crossbar.py
@@ -1,9 +1,9 @@
-def currents(voltages, resistances, r=0):
+def currents(voltages, resistances, r_i=0):
     (num_inputs, num_outputs) = resistances.shape
     num_examples = voltages.shape[1]
-    R = fill_R(resistances)
-    V = fill_V(voltages)
-    I = solve(R, V)
-    currents = extract_currents(I)
+    r = fill_r(resistances, r_i)
+    v = fill_b(voltages)
+    i = solve(r, v)
+    currents = extract_currents(i)
 
     return currents
