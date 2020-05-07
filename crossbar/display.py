d3234ceee5cbf31da7f280f6452f136d46012f5b
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-07 13:13:29 +0100

    create gap

--- crossbar/display.py
+++ crossbar/display.py
@@ -6,3 +6,8 @@ def reduced(original_shape, new_shape):
         original_shape_str = str(original_shape[0]) + 'x' + str(original_shape[1])
         new_shape_str = str(new_shape[0]) + 'x' + str(new_shape[1])
         print('Size of effective resistances matrix was reduced from ' + original_shape_str + ' to ' + new_shape_str + '.')
+
+
+def gap(size=5):
+    gap_str = 5*' '
+    print(gap_str, end='')
