0d409edcc0cdfa6d2e45d0911700024d72d6b7d1
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-07 09:42:00 +0100

    ranem fill.fill_r to fill.r

--- crossbar.py
+++ crossbar.py
@@ -4,7 +4,7 @@ import fill
 
 
 def currents(voltages, resistances, r_i=0):
-    r = fill.fill_r(resistances, r_i)
+    r = fill.r(resistances, r_i)
     v = fill.fill_v(voltages, resistances)
     i = solve(r, v)
     output_currents = extract.currents(i, resistances)
--- fill.py
+++ fill.py
@@ -4,12 +4,12 @@ import kcl
 from scipy.sparse import lil_matrix
 
 
-def fill_r(resistances, r_i):
+def r(resistances, r_i):
     r_shape = tuple(3*resistances.size for _ in range(2))
-    r = lil_matrix(r_shape)
-    r = kvl.apply(r, resistances, r_i)
-    r = kcl.apply(r, resistances)
-    return r
+    r_matrix = lil_matrix(r_shape)
+    r_matrix = kvl.apply(r_matrix, resistances, r_i)
+    r_matrix = kcl.apply(r_matrix, resistances)
+    return r_matrix
 
 
 def fill_v(voltages, resistances):
