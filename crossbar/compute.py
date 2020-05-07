cbcf37b2e664a4df1e24924a970f48c8848ef406
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-07 19:10:09 +0100

    allow to extract all current in currents()

--- crossbar/compute.py
+++ crossbar/compute.py
@@ -2,12 +2,13 @@ from crossbar import solve, extract, fill
 import numpy as np
 
 
-def currents(voltages, resistances, r_i=0):
+def currents(voltages, resistances, r_i=0, extract_all=False):
     """Computes output currents for a crossbar.
 
     :param voltages: Applied voltages. Voltages must be supplied in an array of shape m x p, where m is the number of word lines and p is the number of examples (sets of voltages applied one by one).
     :param resistances: Resistances of crossbar devices. Resistances must be supplied in an array of shape m x n, where n is the number of bit lines.
     :param r_i: Interconnect resistance. It is assumed that all interconnects have the same resistance.
+    :param extract_all: If True, extracts not only the output currents, but also the currents in all the branches of a crossbar.
     :return: Output currents. Currents are returned in an array of shape p x n.
     """
     complete_output_currents = np.zeros((voltages.shape[1], resistances.shape[1]))
@@ -15,7 +16,7 @@ def currents(voltages, resistances, r_i=0):
     r = fill.r(resistances, r_i)
     v = fill.v(voltages, resistances)
     i = solve.i(r, v)
-    output_currents, device_currents, horizontal_currents, vertical_currents = extract.currents(i, resistances)
+    output_currents, device_currents, horizontal_currents, vertical_currents = extract.currents(i, resistances, extract_all=extract_all)
     complete_output_currents[:, :output_currents.shape[1]] = output_currents
 
     return output_currents, device_currents, horizontal_currents, vertical_currents
