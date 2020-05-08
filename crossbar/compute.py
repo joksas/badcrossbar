68ad69f0e4cc8eec14ad9c3a34711b588f304d14
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-08 19:58:22 +0100

    clarify what currents() returns

--- crossbar/compute.py
+++ crossbar/compute.py
@@ -7,7 +7,7 @@ def currents(voltages, resistances, r_i=0, **kwargs):
     :param voltages: Applied voltages. Voltages must be supplied in an array of shape m x p, where m is the number of word lines and p is the number of examples (sets of voltages applied one by one).
     :param resistances: Resistances of crossbar devices. Resistances must be supplied in an array of shape m x n, where n is the number of bit lines.
     :param r_i: Interconnect resistance. It is assumed that all interconnects have the same resistance.
-    :return: Output currents. Currents are returned in an array of shape p x n.
+    :return: Currents in a crossbar. Output currents are returned in an array of shape p x n. If extract_all=False is not passed, then also currents flowing through crossbar devices, word lines and bit lines are returned as lists of arrays of shape m x n.
     """
     # TODO: check that shapes are correct
     original_shape = extract.shapes(voltages, resistances)
