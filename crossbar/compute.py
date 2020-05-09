0288fa32af375be49d1c4a0d57c84dfa75b23368
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-09 19:05:30 +0100

    use check.crossbar_requirements()

--- crossbar/compute.py
+++ crossbar/compute.py
@@ -1,4 +1,4 @@
-from crossbar import solve, extract, fill
+from crossbar import solve, extract, fill, check
 
 
 def currents(voltages, resistances, r_i=0, **kwargs):
@@ -9,7 +9,7 @@ def currents(voltages, resistances, r_i=0, **kwargs):
     :param r_i: Interconnect resistance. It is assumed that all interconnects have the same resistance.
     :return: Currents in a crossbar. Output currents are returned in an array of shape p x n. If extract_all=False is not passed, then also currents flowing through crossbar devices, word lines and bit lines are returned as lists of arrays of shape m x n.
     """
-    # TODO: check that shapes are correct
+    resistances, voltages = check.crossbar_requirements(resistances, voltages)
     original_shape = extract.shapes(voltages, resistances)
     resistances, voltages = extract.reduced_resistances(resistances, voltages)
     r = fill.r(resistances, r_i)
