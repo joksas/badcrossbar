92cf7edb81a33e1a6d1bf4739b27bce75689883b
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-09 19:01:47 +0100

    create match_shape()

--- crossbar/check.py
+++ crossbar/check.py
@@ -4,7 +4,7 @@ import numpy as np
 def crossbar_requirements(resistances, voltages):
     resistances, voltages = matrix_type(resistances, voltages)
     empty(resistances, voltages)
-    match_shape()
+    match_shape((resistances, 0), (voltages, 0))
 
     return resistances, voltages
 
@@ -31,3 +31,10 @@ def empty(*args):
     for arg in args:
         if arg.size == 0:
             raise ValueError('Array is empty!')
+
+
+def match_shape(*args):
+    dim = args[0][0].shape[args[0][1]]
+    for arg in args:
+        if arg[0].shape[arg[1]] != dim:
+            raise ValueError('Array shapes do not match!')
