396b6df55015245cdcb1bc264d9bd917268633f9
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-12 13:16:48 +0100

    use sparse matrix

--- badcrossbar/fill.py
+++ badcrossbar/fill.py
@@ -8,7 +8,7 @@ def g(resistances, r_i):
         g_shape = tuple(resistances.shape[0] + resistances.shape[1] for _ in range(2))
     else:
         g_shape = tuple(2*resistances.size for _ in range(2))
-    g_matrix = np.zeros(g_shape)
+    g_matrix = lil_matrix(g_shape)
     g_matrix = kcl.apply(g_matrix, resistances, r_i)
     return g_matrix
 
