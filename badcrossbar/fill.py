16c213d2c696697dba53800323a59feb8396eb40
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-12 11:22:29 +0100

    delete v

--- badcrossbar/fill.py
+++ badcrossbar/fill.py
@@ -3,22 +3,6 @@ from badcrossbar import kcl, display
 from scipy.sparse import lil_matrix
 
 
-def v(voltages, resistances):
-    """Fills matrix v with values.
-
-    This function fills matrix v (that will be used to solve matrix equation ri = v) with values.
-
-    :param voltages: Applied voltages.
-    :param resistances: Resistances of crossbar devices.
-    :return: Filled v matrix.
-    """
-    v_shape = (3*resistances.size, voltages.shape[1])
-    v_matrix = np.zeros(v_shape)
-    v_matrix[:resistances.size, :] = np.repeat(voltages, resistances.shape[1], axis=0)
-    display.message('v matrix filled.')
-    return v_matrix
-
-
 def g(resistances, r_i):
     if r_i == 0:
         g_shape = tuple(resistances.shape[0] + resistances.shape[1] for _ in range(2))
