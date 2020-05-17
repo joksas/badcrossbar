c0994247cfd0a9b483174c054ff7c0d18574f4fe
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-17 08:59:37 +0100

    use extracted qucs data

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -1,240 +1,31 @@
 import badcrossbar
 import numpy as np
+import pickle
 from collections import namedtuple
 import pytest
 
 
-@pytest.mark.parametrize('file_name', ['2x3_a', '2x3_c', '2x3_d', '2x1_a', '2x1_b'])
+@pytest.mark.parametrize('file_name', ['1x4_a', '1x4_b', '1x4_c', '1x4_d', '5x1_a', '5x1_b' '5x1_c', '5x4_a', '5x4_b', '5x4_c', '5x4_d', '5x4_e', '5x4_f', '5x4_g'])
 def test_currents_qucs(file_name):
-    resistances, voltages, r_i, expected_currents = qucs_data(file_name)
-    computed_currents = badcrossbar.compute(voltages, resistances, r_i=r_i).currents
-    compare_currents(computed_currents, expected_currents)
-
-
-# def test_currents_qucs_2x3_b():
-#     """Tests outputs of badcrossbar.compute.test_currents against results from Qucs. See qucs-files/qucs_2x3_b.sch.
-#
-#     :return: None.
-#     """
-#     Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
-#     resistances = np.array([[10, 20, 30],
-#                             [40, 50, 60]])
-#     voltages = np.array([[3],
-#                          [5]])
-#     r_i = 0
-#
-#     expected_currents = Currents(np.array([[0.425, 0.25, 0.183333333333333]]),
-#                                  np.array([[0.3, 0.15, 0.1],
-#                                            [0.125, 0.1, 0.0833333333333]]),
-#                                  np.array([[0.55, 0.25, 0.1],
-#                                            [0.3083333333333, 0.183333333333, 0.08333333333333]]),
-#                                  np.array([[0.3, 0.15, 0.1],
-#                                            [0.425, 0.25, 0.183333333333333]]))
-#     computed_currents = badcrossbar.compute(voltages, resistances, r_i=r_i).currents
-#
-#     compare_currents(computed_currents, expected_currents)
-
-
-# def test_currents_qucs_2x1_c():
-#     """Tests outputs of badcrossbar.compute.test_currents against results from Qucs. See qucs-files/qucs_2x1_c.sch.
-#
-#     :return: None.
-#     """
-#     Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
-#     resistances = np.array([[1],
-#                             [np.inf]])
-#     voltages = np.array([[50],
-#                          [5]])
-#     r_i = 0
-#
-#     expected_currents = Currents(np.array([[50]]),
-#                                  np.array([[50],
-#                                            [0]]),
-#                                  np.array([[50],
-#                                            [0]]),
-#                                  np.array([[50],
-#                                            [50]]))
-#     computed_currents = badcrossbar.compute(voltages, resistances, r_i=r_i).currents
-#
-#     compare_currents(computed_currents, expected_currents)
-
-
-# def test_currents_dot_product_engine():
-#     """Tests whether crossbars behave as dot product engines when r_i = 0.
-#
-#     When r_i = 0, crossbars should be able to compute matrix-vector products of conductances of devices and applied voltages.
-#
-#     :return: None.
-#     """
-#     for _ in range(10):
-#         m, n, p = np.random.randint(1, 30, 3)
-#         resistances = np.random.rand(m, n)
-#         voltages = np.random.rand(m, p)
-#         r_i = 0
-#         computed_currents = badcrossbar.compute(voltages, resistances, r_i).currents
-#
-#         conductances = np.reciprocal(resistances)
-#         expected_output = np.dot(np.transpose(voltages), conductances)
-#
-#         np.testing.assert_array_almost_equal(computed_currents.output, expected_output)
-
-
-# def test_currents_dot_product_engine_insulating():
-#     """Tests whether crossbars behave as dot product engines when r_i = 0, but some devices are insulating.
-#
-#     When r_i = 0, crossbars should be able to compute matrix-vector products of conductances of devices and applied voltages.
-#
-#     :return: None.
-#     """
-#     for _ in range(10):
-#         m, n, p = np.random.randint(1, 30, 3)
-#         resistances = np.random.rand(m, n)
-#         indices = np.random.choice(range(m*n), int(0.3*m*n), replace=False)
-#         rows = (indices/n).astype(int)
-#         columns = indices - n*rows
-#         resistances[rows, columns] = np.inf  # makes 30% of random devices insulating
-#
-#         voltages = np.random.rand(m, p)
-#         r_i = 0
-#         computed_currents = badcrossbar.compute(voltages, resistances, r_i).currents
-#
-#         conductances = np.reciprocal(resistances)
-#         expected_output = np.dot(np.transpose(voltages), conductances)
-#
-#         np.testing.assert_array_almost_equal(computed_currents.output, expected_output)
-
-
-# def test_currents_dot_product_engine_insulating_lines():
-#     """Tests whether crossbars behave as dot product engines when r_i = 0, but some word lines and bit lines contain only insulating devices.
-#
-#     When r_i = 0, crossbars should be able to compute matrix-vector products of conductances of devices and applied voltages.
-#
-#     :return: None.
-#     """
-#     for _ in range(10):
-#         m, n, p = np.random.randint(10, 30, 3)
-#         resistances = np.random.rand(m, n)
-#         rows = np.random.choice(range(m), int(0.3*m), replace=False)
-#         columns = np.random.choice(range(n), int(0.3*n), replace=False)
-#         resistances[rows, :] = np.inf  # makes 30% of random word lines contain only insulating devices
-#         resistances[:, columns] = np.inf  # makes 30% of random bit lines contain only insulating devices
-#
-#         voltages = np.random.rand(m, p)
-#         r_i = 0
-#         computed_currents = badcrossbar.compute(voltages, resistances, r_i).currents
-#
-#         conductances = np.reciprocal(resistances)
-#         expected_output = np.dot(np.transpose(voltages), conductances)
-#
-#         np.testing.assert_array_almost_equal(computed_currents.output, expected_output)
-
-
-# def test_currents_return_only_outputs():
-#     """Tests whether the function returns only output currents and whether all the other parameters are None.
-#
-#     :return: None.
-#     """
-#     m, n, p = np.random.randint(10, 30, 3)
-#     resistances = np.random.rand(m, n)
-#     voltages = np.random.rand(m, p)
-#     r_i = 0
-#     computed_currents = badcrossbar.compute(voltages, resistances, r_i, extract_all=False).currents
-#
-#     assert type(computed_currents.output) is np.ndarray
-#     assert computed_currents.device is None
-#     assert computed_currents.word_line is None
-#     assert computed_currents.bit_line is None
-
-
-# def test_currents_qucs_2x3_a_only_outputs():
-#     """Tests output parameter of the output badcrossbar.compute.test_currents against results from Qucs, when passed an argument extract_all=False. See qucs-files/qucs_2x3_a.sch.
-#
-#     :return: None.
-#     """
-#     resistances = np.array([[10, 20, 30],
-#                             [40, 50, 60]])
-#     voltages = np.array([[3],
-#                          [5]])
-#     r_i = 0.1
-#
-#     expected_output = np.array([[0.4109446865, 0.2427676365, 0.1782942773]])
-#     computed_currents = badcrossbar.compute(voltages, resistances, r_i=r_i, extract_all=False).currents
-#
-#     np.testing.assert_array_almost_equal(computed_currents.output, expected_output)
+    resistances, voltages, r_i, expected_solution = qucs_data(file_name)
+    computed_solution = badcrossbar.compute(voltages, resistances, r_i=r_i)
+    compare_currents(computed_solution.currents, expected_solution.currents)
 
 
 def qucs_data(file_name):
+    Solution = namedtuple('Solution', ['currents', 'voltages'])
     Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
+    Voltages = namedtuple('Voltages', ['word_line', 'bit_line'])
 
-    if file_name == '2x3_a':
-        resistances = np.array([[10, 20, 30],
-                                [40, 50, 60]])
-        voltages = np.array([[3],
-                             [5]])
-        r_i = 0.1
-        expected_currents = Currents(np.array([[0.4109446865, 0.2427676365, 0.1782942773]]),
-                                     np.array([[0.2877316742, 0.1442221472, 0.09620240051],
-                                               [0.1232130123, 0.09854548924, 0.08209187684]]),
-                                     np.array([[0.5281562219, 0.2404245477, 0.09620240051],
-                                               [0.3038503784, 0.1806373661, 0.08209187684]]),
-                                     np.array([[0.2877316742, 0.1442221472, 0.09620240051],
-                                               [0.4109446865, 0.2427676365, 0.1782942773]]))
-    elif file_name == '2x3_c':
-        resistances = np.array([[45, np.inf, np.inf],
-                                [150, np.inf, 20]])
-        voltages = np.array([[14],
-                             [6]])
-        r_i = 1.5
-        expected_currents = Currents(np.array([[0.3160015477, 0, 0.228795581]]),
-                                     np.array([[0.2817916183, 0, 0],
-                                               [0.03420992942, 0, 0.228795581]]),
-                                     np.array([[0.2817916183, 0, 0],
-                                               [0.2630055104, 0.228795581, 0.228795581]]),
-                                     np.array([[0.2817916183, 0, 0],
-                                               [0.3160015477, 0, 0.228795581]]))
-    elif file_name == '2x3_d':
-        resistances = np.array([[np.inf, np.inf, np.inf],
-                                [30, 45.5, np.inf]])
-        voltages = np.array([[10],
-                             [7]])
-        r_i = 0.5
-        expected_currents = Currents(np.array([[0.2234425948, 0.1465591213, 0]]),
-                                     np.array([[0, 0, 0],
-                                               [0.2234425948, 0.1465591213, 0]]),
-                                     np.array([[0, 0, 0],
-                                               [0.3700017161, 0.1465591213, 0]]),
-                                     np.array([[0, 0, 0],
-                                               [0.2234425948, 0.1465591213, 0]]))
-    elif file_name == '2x1_a':
-        resistances = np.array([[60],
-                                [10]])
-        voltages = np.array([[3],
-                             [0.1]])
-        r_i = 1
+    path = 'qucs-files/' + file_name + '.pickle'
+    with open(path, 'rb') as handle:
+        resistances, voltages, r_i, i_o, i_d, i_w, i_b, v_w, v_b = pickle.load(handle)
 
-        expected_currents = Currents(np.array([[0.0519205298]]),
-                                     np.array([[0.04754966887],
-                                               [0.004370860927]]),
-                                     np.array([[0.04754966887],
-                                               [0.004370860927]]),
-                                     np.array([[0.04754966887],
-                                               [0.0519205298]]))
-    elif file_name == '2x1_b':
-        resistances = np.array([[100],
-                                [500]])
-        voltages = np.array([[0],
-                             [0]])
-        r_i = 1
+    extracted_currents = Currents(i_o, i_d, i_w, i_b)
+    extracted_voltages = Voltages(v_w, v_b)
+    solution = Solution(extracted_currents, extracted_voltages)
 
-        expected_currents = Currents(np.array([[0]]),
-                                     np.array([[0],
-                                               [0]]),
-                                     np.array([[0],
-                                               [0]]),
-                                     np.array([[0],
-                                               [0]]))
-
-    return resistances, voltages, r_i, expected_currents
+    return resistances, voltages, r_i, solution
 
 
 def compare_currents(computed_currents, expected_currents):
