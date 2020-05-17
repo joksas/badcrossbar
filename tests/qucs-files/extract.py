9f745c4ab0facc30d5a29ce79646043fe9d041dd
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-17 08:01:15 +0100

    create save_file()

--- tests/qucs-files/extract.py
+++ tests/qucs-files/extract.py
@@ -1,20 +1,21 @@
 import numpy as np
+import os
+import pickle
 from collections import namedtuple
 
 
+Solution = namedtuple('Solution', ['currents', 'voltages'])
+Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
+Voltages = namedtuple('Voltages', ['word_line', 'bit_line'])
+
+
 def extract(filename, shape):
-    data = open_file(filename)
+    data = open_file(filename, 'dat')
     R = two_dim(data, shape, var1='R')
     V = one_dim(data, shape, var1='V')
     S = solution(data, shape)
     r_i = zero_dim(data, var1='r_i')
-    return R, V, r_i, S
-
-
-def open_file(filename):
-    with open(filename, 'r') as opened_file:
-        contents = opened_file.read()
-    return contents
+    save_file((R, V, r_i, S), filename, allow_overwrite=True)
 
 
 def two_dim(data, shape, var1, var2=None):
@@ -57,18 +58,14 @@ def zero_dim(data, var1, var2=None):
 
 
 def solution(data, shape):
-    Solution = namedtuple('Solution', ['currents', 'voltages'])
-
     device_currents = two_dim(data, shape, var1='d', var2='.I')
     word_line_currents = two_dim(data, shape, var1='w', var2='.I')
     bit_line_currents = two_dim(data, shape, var1='b', var2='.I')
     output_currents = bit_line_currents[-1, :]
-    Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
     extracted_currents = Currents(output_currents, device_currents, word_line_currents, bit_line_currents)
 
     word_line_voltages = two_dim(data, shape, var1='w', var2='.V')
     bit_line_voltages = two_dim(data, shape, var1='b', var2='.V')
-    Voltages = namedtuple('Currents', ['word_line', 'bit_line'])
     extracted_voltages = Voltages(word_line_voltages, bit_line_voltages)
 
     solution = Solution(extracted_currents, extracted_voltages)
@@ -78,3 +75,35 @@ def solution(data, shape):
 
 def between(data, delim1, delim2):
     return data.split(delim1)[1].split(delim2)[0]
+
+
+def open_file(filename, extension):
+    with open(filename + '.' + extension, 'r') as opened_file:
+        contents = opened_file.read()
+    return contents
+
+
+def save_file(variable, path, allow_overwrite=False, verbose=False):
+    if allow_overwrite is True:
+        path += '.pickle'
+        with open(path, 'wb') as handle:
+            pickle.dump(variable, handle, protocol=pickle.HIGHEST_PROTOCOL)
+        if verbose is True:
+            print('Saving ' + str(path))
+    else:
+        counter = 1
+        file_saved = False
+        new_path = path
+
+        while file_saved is False:
+            full_path = new_path + '.pickle'
+
+            if os.path.exists(full_path) is False:
+                with open(full_path, 'wb') as handle:
+                    pickle.dump(variable, handle, protocol=pickle.HIGHEST_PROTOCOL)
+                if verbose is True:
+                    print('Saving ' + str(full_path))
+                file_saved = True
+            else:
+                new_path = path + '_' + str(counter)
+                counter += 1
