8172d47446e4492c943849d9b9707f7c7fa26322
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-20 18:06:43 +0100

    remove i()

--- badcrossbar/nonideal/solve.py
+++ badcrossbar/nonideal/solve.py
@@ -1,20 +1,5 @@
 from scipy.sparse import linalg
 from badcrossbar import display
-from badcrossbar.nonideal import extract
-
-
-def i(r, v):
-    """Solves matrix equation ri = v.
-
-    :param r: r matrix.
-    :param v: v matrix.
-    :return: Currents in each branch of the crossbar.
-    """
-    display.message('Started solving for i.')
-    r = extract.non_infinite(r)
-    i_matrix = linalg.spsolve(r.tocsc(), v)  # converts lil_matrix to csc_matrix before solving
-    display.message('Solved for i.')
-    return i_matrix
 
 
 def v(g, i):
