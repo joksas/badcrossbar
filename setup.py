157d7696e1ff0a3b1d97681fe644e828b59de2ce
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-22 12:17:56 +0100

    create

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='badcrossbar',
+    version='1.0.0',
+    packages=['badcrossbar', 'badcrossbar.ideal', 'badcrossbar.nonideal', 'tests'],
+    url='https://github.com/joksas/badcrossbar',
+    license='',
+    author='Dovydas Joksas',
+    author_email='dovydas.joksas.15@ucl.ac.uk',
+    description=''
+)
