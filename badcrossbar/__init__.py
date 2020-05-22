e7f36ff2450820a98ff2cb7d28d4c9100a095fe7
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-22 12:30:56 +0100

    move description from __init__.py to setup.py

--- badcrossbar/__init__.py
+++ badcrossbar/__init__.py
@@ -1,7 +1,1 @@
-"""
-In perfect conditions, crossbar array can work as a dot product engine computing matrix products of applied voltages and the conductances of crossbar devices. However, when interconnect resistance is non-zero, crossbars deviate from this behaviour. This package computes output currents for arbitrary values of crossbar devices' resistances, applied voltages and interconnect resistance.
-
-For most users, function badcrossbar.compute should complete the main task at hand, i.e. computing crossbar currents and voltages.
-"""
-
 from .compute import compute
