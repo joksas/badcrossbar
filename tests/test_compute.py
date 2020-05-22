6311cd858140c6d25c5d97df5d565050539de92e
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-22 11:40:48 +0100

    add tests that use two input examples at a time

--- tests/test_compute.py
+++ tests/test_compute.py
@@ -6,23 +6,26 @@ import pytest
 
 
 file_names = ['1x1_a', '1x1_b', '1x1_c', '1x1_d', '1x1_e', '1x4_a', '1x4_b', '1x4_c', '1x4_d', '1x4_e', '5x1_a', '5x1_b', '5x1_c', '5x4_a', '5x4_b', '5x4_c', '5x4_d', '5x4_e', '5x4_f', '5x4_g', '5x4_h']
+double_inputs = [False, True]
 
 
 @pytest.mark.parametrize('file_name', file_names)
-def test_currents_qucs(file_name):
-    resistances, voltages, r_i, expected_solution = qucs_data(file_name)
+@pytest.mark.parametrize('double_input', double_inputs)
+def test_currents_qucs(file_name, double_input):
+    resistances, voltages, r_i, expected_solution = qucs_data(file_name, double_input=double_input)
     computed_solution = badcrossbar.compute(voltages, resistances, r_i=r_i)
     compare_currents(computed_solution.currents, expected_solution.currents)
 
 
 @pytest.mark.parametrize('file_name', file_names)
-def test_voltages_qucs(file_name):
-    resistances, voltages, r_i, expected_solution = qucs_data(file_name)
+@pytest.mark.parametrize('double_input', double_inputs)
+def test_voltages_qucs(file_name, double_input):
+    resistances, voltages, r_i, expected_solution = qucs_data(file_name, double_input=double_input)
     computed_solution = badcrossbar.compute(voltages, resistances, r_i=r_i)
     compare_voltages(computed_solution.voltages, expected_solution.voltages)
 
 
-def qucs_data(file_name):
+def qucs_data(file_name, double_input=False):
     Solution = namedtuple('Solution', ['currents', 'voltages'])
     Currents = namedtuple('Currents', ['output', 'device', 'word_line', 'bit_line'])
     Voltages = namedtuple('Voltages', ['word_line', 'bit_line'])
@@ -31,8 +34,13 @@ def qucs_data(file_name):
     with open(path, 'rb') as handle:
         resistances, voltages, r_i, i_o, i_d, i_w, i_b, v_w, v_b = pickle.load(handle)
 
-    extracted_currents = Currents(i_o, i_d, i_w, i_b)
-    extracted_voltages = Voltages(v_w, v_b)
+    if double_input is False:
+        extracted_currents = Currents(i_o, i_d, i_w, i_b)
+        extracted_voltages = Voltages(v_w, v_b)
+    else:
+        extracted_currents = Currents(np.concatenate((i_o, i_o)), [i_d]*2, [i_w]*2, [i_b]*2)
+        extracted_voltages = Voltages([v_w]*2, [v_b]*2)
+        voltages = np.concatenate((voltages, voltages), axis=1)
     solution = Solution(extracted_currents, extracted_voltages)
 
     return resistances, voltages, r_i, solution
