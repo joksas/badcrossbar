e564f2be64a973080d2236c566e06ce362ce6e2f
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-05-23 12:41:12 +0100

    update description

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 """
-In perfect conditions, crossbar array can work as a dot product engine computing matrix products of applied voltages and the conductances of crossbar devices. However, when interconnect resistance is non-zero, crossbars deviate from this behaviour. This package computes output currents for arbitrary values of crossbar devices' resistances, applied voltages and interconnect resistance.
+In perfect conditions, crossbar array can work as a dot product engine computing matrix products of applied voltages and the conductances of crossbar devices. However, when interconnect resistance is non-zero, crossbars deviate from this behaviour. This package computes branch currents and node voltages of the crossbar for arbitrary values of devices' resistances, applied voltages and interconnect resistance.
 
 For most users, function badcrossbar.compute should complete the main task at hand, i.e. computing crossbar currents and voltages.
 """
