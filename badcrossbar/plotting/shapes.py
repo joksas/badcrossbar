67b1efe3068a2a5f60b93153a266a8bfec7573b0
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-09 10:03:26 +0100

    create semicircle()

--- badcrossbar/plotting/shapes.py
+++ badcrossbar/plotting/shapes.py
@@ -1,4 +1,5 @@
 import cairo
+import numpy as np
 
 
 def line(context, length, angle=0):
@@ -16,4 +17,24 @@ def line(context, length, angle=0):
     """
     context.rotate(angle)
     context.rel_line_to(length, 0)
+    context.rotate(-angle)
+
+
+def semicircle(context, diameter, angle=0):
+    """Draws a semicircle at a specified angle.
+
+    Parameters
+    ----------
+    context : cairo.Context
+        Context.
+    diameter : float
+        Diameter of the semicircle.
+    angle : float
+        Angle in radians of the rotation of plane from the positive x axis
+        towards positive y axis.
+    """
+    context.rotate(angle)
+    x, y = context.get_current_point()
+    radius = diameter/2
+    context.arc(x+radius, y, radius, np.pi, 2*np.pi)
     context.rotate(-angle)
