42acf111e5658bc66789d636fbd49bf0c6f9cf40
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-09 19:24:55 +0100

    create get()

---
+++ badcrossbar/plotting/dimensions.py
@@ -0,0 +1,26 @@
+def get(shape, max_dimension=1000, color_bar_fraction=(0.5, 0.15),
+        border=0.05):
+    adjusted_shape = (shape[0]+0.5, shape[1]+0.5)
+    active_horizontal_fraction = 1 - color_bar_fraction[1] - 2 * border
+    if adjusted_shape[1]/adjusted_shape[0] > active_horizontal_fraction:
+        width_fraction = active_horizontal_fraction
+        segment_fraction = width_fraction/adjusted_shape[1]
+        height_fraction = segment_fraction*adjusted_shape[0]
+        width = max_dimension
+        height = (height_fraction + 2*border)*max_dimension
+    else:
+        height_fraction = 1 - 2*border
+        segment_fraction = height_fraction/adjusted_shape[0]
+        width_fraction = segment_fraction*adjusted_shape[1]
+        height = max_dimension
+        width = (width_fraction + color_bar_fraction[1] + 2*border) * max_dimension
+
+    if height/width < color_bar_fraction[0]:
+        height = max_dimension * color_bar_fraction[0]
+
+    segment_length = segment_fraction*max_dimension
+
+    x_start = border*max_dimension
+    y_start = height/2 - adjusted_shape[0]*segment_length/2
+
+    return width, height, segment_length, x_start, y_start
