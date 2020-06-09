4a9b46321cd48277aeaab0b45a263df3b76c745b
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-09 13:24:51 +0100

    create device_row()

--- badcrossbar/plotting/crossbar.py
+++ badcrossbar/plotting/crossbar.py
@@ -1,5 +1,6 @@
 import numpy as np
 import badcrossbar.plotting.shapes as shapes
+import badcrossbar.plotting.devices as devices
 import badcrossbar.plotting.utils as utils
 
 
@@ -11,7 +12,7 @@ def word_line(context, colors, width=1, segment_length=120, first=False):
     context : cairo.Context
         Context.
     colors : list of tuple of int
-        Colors of the bit line segments in RGB (max value of 255).
+        Colors of the word line segments in RGB (max value of 255).
     width : float
         Width of the path.
     segment_length : float
@@ -47,4 +48,27 @@ def bit_line(context, colors, width=1, segment_length=120):
     """
     for color in colors:
         shapes.line(context, segment_length, angle=np.pi/2)
+        utils.complete_path(context, rgb=color, width=width)
+
+
+def device_row(context, colors, width=5, segment_length=120):
+    """Draws a row of crossbar devices.
+
+    Parameters
+    ----------
+    context : cairo.Context
+        Context.
+    colors : list of tuple of int
+        Colors of the crossbar devices in RGB (max value of 255).
+    width : float
+        Width of the path.
+    segment_length : float
+        The length of each segment.
+    """
+    x, y = context.get_current_point()
+    device_length = segment_length/2*np.sqrt(2)  # Pythagorean theorem
+    for color in colors:
+        x += segment_length
+        context.move_to(x, y)
+        devices.memristor(context, length=device_length, angle=np.pi/4)
         utils.complete_path(context, rgb=color, width=width)
