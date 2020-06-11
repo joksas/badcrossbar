e06750268e65e9dbc1f8e34fe53168fcd6faa304
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-11 10:26:11 +0100

    distribute draw() into different functions

--- badcrossbar/plotting/color_bar.py
+++ badcrossbar/plotting/color_bar.py
@@ -4,89 +4,9 @@ from badcrossbar import plotting as plotting
 
 
 def draw(ctx, color_bar_dims, low, high):
-    ctx.rectangle(*color_bar_dims)
-    pattern = cairo.LinearGradient(color_bar_dims[0], color_bar_dims[1],
-                                   color_bar_dims[0] + color_bar_dims[2],
-                                   color_bar_dims[1] + color_bar_dims[3])
-
-    top_rgb = plotting.utils.rgb_interpolation(
-        np.array([high]), low=low, high=high)[0]
-    if low != high:
-        middle_rgb = plotting.utils.rgb_interpolation(
-            np.array([0]), low=low, high=high)[0]
-    else:
-        middle_rgb = plotting.utils.rgb_interpolation(
-            np.array([0]), low=-1, high=1)[0]
-    bottom_rgb = plotting.utils.rgb_interpolation(
-        np.array([low]), low=low, high=high)[0]
-
-    ctx.set_source_rgb(0, 0, 0)
-    font_size = color_bar_dims[2]/2.5
-    ctx.set_font_size(font_size)
-
-    if low < 0 < high:
-        pattern.add_color_stop_rgb(0, *top_rgb)
-        pattern.add_color_stop_rgb(0.5, *middle_rgb)
-        pattern.add_color_stop_rgb(1, *bottom_rgb)
-
-        x = color_bar_dims[0] + color_bar_dims[2]*1.2
-        y = color_bar_dims[1] + 0.5*font_size
-        ctx.move_to(x, y)
-        ctx.show_text(str(high))
-
-        x = color_bar_dims[0] + color_bar_dims[2]*1.2
-        y = color_bar_dims[1] + 0.5*color_bar_dims[3] + 0.5*font_size
-        ctx.move_to(x, y)
-        ctx.show_text(str(0))
-
-        x = color_bar_dims[0] + color_bar_dims[2]*1.2
-        y = color_bar_dims[1] + color_bar_dims[3] + 0.5*font_size
-        ctx.move_to(x, y)
-        ctx.show_text(str(low))
-    else:
-        if high > 0:
-            pattern.add_color_stop_rgb(0, *top_rgb)
-            pattern.add_color_stop_rgb(1, *middle_rgb)
-
-            x = color_bar_dims[0] + color_bar_dims[2]*1.2
-            y = color_bar_dims[1] + 0.5*font_size
-            ctx.move_to(x, y)
-            ctx.show_text(str(high))
-
-            x = color_bar_dims[0] + color_bar_dims[2]*1.2
-            y = color_bar_dims[1] + color_bar_dims[3] + 0.5*font_size
-            ctx.move_to(x, y)
-            ctx.show_text(str(0))
-        else:
-            pattern.add_color_stop_rgb(0, *middle_rgb)
-            pattern.add_color_stop_rgb(1, *bottom_rgb)
-
-            x = color_bar_dims[0] + color_bar_dims[2]*1.2
-            y = color_bar_dims[1] + 0.5*font_size
-            ctx.move_to(x, y)
-            ctx.show_text(str(0))
-
-            x = color_bar_dims[0] + color_bar_dims[2]*1.2
-            y = color_bar_dims[1] + color_bar_dims[3] + 0.5*font_size
-            ctx.move_to(x, y)
-            ctx.show_text(str(low))
-
-    ctx.set_source(pattern)
-    ctx.fill()
-
-    font_size *= 1.5
-    ctx.set_font_size(font_size)
-    ctx.set_source_rgb(0, 0, 0)
-    angle = np.pi/2
-
-    _, _, width, height, _, _ = ctx.text_extents('Current (A)')
-    x = color_bar_dims[0] + 2*color_bar_dims[2] + height
-    y = color_bar_dims[1] + 0.5*color_bar_dims[3] + 0.5*width
-    ctx.move_to(x, y)
-
-    ctx.rotate(-angle)
-    ctx.show_text('Current (A)')
-    ctx.rotate(angle)
+    middle = rectangle(ctx, color_bar_dims, low, high)
+    tick_labels(ctx, middle, low, high, color_bar_dims)
+    axis_label(ctx, color_bar_dims)
 
 
 def dimensions(surface_dims, color_bar_fraction, border_fraction):
@@ -160,7 +80,7 @@ def rectangle(ctx, color_bar_dims, low, high):
     ----------
     ctx : cairo.Context
         Context.
-    color_bar_dims : tuple of tuple of int
+    color_bar_dims : tuple of tuple of float
         The first tuple is the top left position of the rectangle, while the
         second tuple represent width and height.
     low : float
