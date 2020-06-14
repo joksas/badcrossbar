3703b0bf24ed7d1b0e4bbfcf1d507997b26d4591
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-14 11:28:46 +0100

    finish

--- examples/computing/single_set_of_inputs.py
+++ examples/computing/single_set_of_inputs.py
@@ -18,6 +18,20 @@ r_i = 0.5
 solution = badcrossbar.compute(applied_voltages, resistances, r_i)
 
 # printing the current through the word line segment to the left of device in
-# the second row and fourth column
+# the second row and fourth column (Python uses zero-based indexing)
 current = solution.currents.word_line[1, 3]
 print('\nThe current through the word line segment is {} A.'.format(current))
+
+# printing the current at the third output (even when only one set of inputs
+# is applied, it is necessary to specify the first index, in this case -- 0)
+current = solution.currents.output[0, 2]
+print('\nThe current at the third output is {} A.'.format(current))
+
+# printing the current through the crossbar device in the last row and the
+# last column ('-1' refers to last element in Python)
+current = solution.currents.device[-1, -1]
+print('\nThe current through the crossbar device is {} A.'.format(current))
+
+# printing the voltage on the bit line node in the last row and second column
+voltage = solution.voltages.bit_line[-1, 1]
+print('\nThe voltage at the bit line node is {} V.'.format(voltage))
