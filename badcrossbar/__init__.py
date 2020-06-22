7c7e0f7768d072987138b6059090f73464991fd1
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-06-22 15:28:57 +0100

    allow to use the package, even if a module cannot be imported

--- badcrossbar/__init__.py
+++ badcrossbar/__init__.py
@@ -1,2 +1,12 @@
-from .compute import compute
-import badcrossbar.plot
+import warnings
+warnings.simplefilter('always', ImportWarning)
+
+try:
+    from .compute import compute
+except ModuleNotFoundError:
+    warnings.warn('Could not import `badcrossbar.compute()`!', ImportWarning)
+
+try:
+    import badcrossbar.plot
+except ModuleNotFoundError:
+    warnings.warn('Could not import `badcrossbar.plot`!', ImportWarning)
