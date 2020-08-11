cfa9aaab93afb1ad332e958fcae414d11bbd98b1
Dovydas <dovydas.joksas.15@ucl.ac.uk>
2020-08-11 10:45:48 +0300

    fix references to fields of named tuples

--- examples/computing/2_multiple_sets_of_inputs.py
+++ examples/computing/2_multiple_sets_of_inputs.py
@@ -19,21 +19,21 @@ solution = badcrossbar.compute(applied_voltages, resistances, r_i)
 # Printing the current through the word line segment to the left of device in
 # the second row and fourth column when the first set of inputs is applied
 # (Python uses zero-based indexing).
-current = solution.branches.word_line[1, 3, 0]
+current = solution.currents.word_line[1, 3, 0]
 print('\nThe current through the word line segment is {} A.'.format(current))
 
 # Printing the current at the third output when the second set of inputs is
 # applied.
-current = solution.branches.output[1, 2]
+current = solution.currents.output[1, 2]
 print('\nThe current at the third output is {} A.'.format(current))
 
 # Printing the current through the crossbar device in the last row and the
 # last column when the last set of inputs is applied (in Python, `-1` refers
 # to the last element in an array).
-current = solution.branches.device[-1, -1, -1]
+current = solution.currents.device[-1, -1, -1]
 print('\nThe current through the crossbar device is {} A.'.format(current))
 
 # Printing the voltage on the bit line node in the last row and second column
 # when the third set of inputs is applied.
-voltage = solution.nodes.bit_line[-1, 1, 2]
+voltage = solution.voltages.bit_line[-1, 1, 2]
 print('\nThe voltage at the bit line node is {} V.'.format(voltage))
