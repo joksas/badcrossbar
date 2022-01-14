d7d2febeece4fe4f97f04a5bdc3202754ea76318
Dovydas Joksas <dovydas@yoshke.org>
2022-01-14 10:54:19 +0000

    use f-strings in example files

--- examples/computing/1_single_set_of_inputs.py
+++ examples/computing/1_single_set_of_inputs.py
@@ -24,18 +24,18 @@ solution = badcrossbar.compute(applied_voltages, resistances, r_i)
 # Printing the current through the word line segment to the left of device in
 # the second row and fourth column (Python uses zero-based indexing).
 current = solution.currents.word_line[1, 3]
-print("\nThe current through the word line segment is {} A.".format(current))
+print(f"\nThe current through the word line segment is ~{current:.3g} A.")
 
 # Printing the current at the third output (even when only one set of inputs
 # is applied, it is necessary to specify the first index, in this case -- `0`).
 current = solution.currents.output[0, 2]
-print("\nThe current at the third output is {} A.".format(current))
+print(f"\nThe current at the third output is ~{current:.3g} A.")
 
 # Printing the current through the crossbar device in the last row and the
 # last column (in Python, `-1` refers to the last element in an array).
 current = solution.currents.device[-1, -1]
-print("\nThe current through the crossbar device is {} A.".format(current))
+print(f"\nThe current through the crossbar device is ~{current:.3g} A.")
 
 # Printing the voltage on the bit line node in the last row and second column.
 voltage = solution.voltages.bit_line[-1, 1]
-print("\nThe voltage at the bit line node is {} V.".format(voltage))
+print(f"\nThe voltage at the bit line node is ~{voltage:.3g} V.")
--- examples/computing/2_multiple_sets_of_inputs.py
+++ examples/computing/2_multiple_sets_of_inputs.py
@@ -24,20 +24,20 @@ solution = badcrossbar.compute(applied_voltages, resistances, r_i)
 # the second row and fourth column when the first set of inputs is applied
 # (Python uses zero-based indexing).
 current = solution.currents.word_line[1, 3, 0]
-print("\nThe current through the word line segment is {} A.".format(current))
+print(f"\nThe current through the word line segment is ~{current:.3g} A.")
 
 # Printing the current at the third output when the second set of inputs is
 # applied.
 current = solution.currents.output[1, 2]
-print("\nThe current at the third output is {} A.".format(current))
+print(f"\nThe current at the third output is ~{current:.3g} A.")
 
 # Printing the current through the crossbar device in the last row and the
 # last column when the last set of inputs is applied (in Python, `-1` refers
 # to the last element in an array).
 current = solution.currents.device[-1, -1, -1]
-print("\nThe current through the crossbar device is {} A.".format(current))
+print(f"\nThe current through the crossbar device is ~{current:.3g} A.")
 
 # Printing the voltage on the bit line node in the last row and second column
 # when the third set of inputs is applied.
 voltage = solution.voltages.bit_line[-1, 1, 2]
-print("\nThe voltage at the bit line node is {} V.".format(voltage))
+print(f"\nThe voltage at the bit line node is ~{voltage:.3g} V.")
